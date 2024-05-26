07b9d23ba7cc92373464a991d7e04c0db76099e3
OP SamuelCC <80846151+Sammmmmmmmmmmmy@users.noreply.github.com>
2024-05-26 23:41:26 +0100

    Update mapcommands.js

--- commands/Embeds/mapcommands.js
+++ commands/Embeds/mapcommands.js
@@ -29,7 +29,7 @@ module.exports = {
         .setTitle("Map Commands")
         .setDescription(`Below are all of the commands avaliable with me!`)
         .addFields(
-            {name: "__Information__", value: "The bot's commands are not case sensitive, meaning both uppercase and lowercase characters will work.\n\nIf a command is providing an incorrect response or not responding please let <@574217755692236803> know in DMs."},
+            {name: "__Information__", value: "The bot's commands are not case sensitive, meaning both uppercase and lowercase characters will work."},
             {name: "__Map Commands__", value: ">v1\n>v2\n>v3\n>v4\n>v5\n>v6\n>v7\n>v8\n>v9\n>v10\n>v11\n>v12\n>v13\n>v14\n>v15\n>v16\n>v17\n>v18\n>v19\n>v20\n>v21\n>v22\n>v23\n>v24\n>v25\n>v26\n>v27\n>v28\n>v29"},
         )
         .setFooter({text: "Bot made by OP SamuelCC • Last Updated At"})
